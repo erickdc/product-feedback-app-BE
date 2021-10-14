f6bb38978b644418ff653e9676d004fb573a60c6
Erick <erickdcb10@gmail.com>
2021-10-13 20:11:13 -0600

    a

--- src/controllers/feedbackController.ts
+++ src/controllers/feedbackController.ts
@@ -1,68 +1,66 @@
 // Import Service
-const feedbackService = require('../services/FeedbackService');
+const feedbackService = require("../services/feedbackService");
 
-class FeedbackController {
-	getFeedback = async (req: any, res: any) => {
-		try {
-			return res.status(200).json(await feedbackService.getFeedback());
-		} catch (err) {
-			console.error(err);
-		}
-	};
-	getSingleFeedback = async (req: any, res: any) => {
-		try {
-			const id = req.params.id;
-			return res.status(200).json(await feedbackService.getSingleFeedback(id));
-		} catch (err) {
-			console.error(err);
-		}
-	};
-	getSingleFeedbackComments = async (req: any, res: any) => {
-		try {
-			const id = req.params.id;
-			return res
-				.status(200)
-				.json(await feedbackService.getSingleFeedbackComments(id));
-		} catch (err) {
-			console.error(err);
-		}
-	};
-	createFeedback = async (req: any, res: any) => {
-		try {
-			const postData = req.body;
-			await feedbackService.createFeedback(postData);
-			return res.status(201).json(feedbackService.getFeedback());
-		} catch (err) {
-			console.error(err);
-		}
-	};
-	updateFeedback = async (req: any, res: any) => {
-		try {
-			const id: string = req.params.id;
-			const postData = req.body;
-			await feedbackService.updateFeedback(postData, id);
-			return res.status(200).json(await feedbackService.getFeedback());
-		} catch (err) {
-			console.error(err);
-		}
-	};
-	deleteFeedback = async (req: any, res: any) => {
-		try {
-			const id: string = req.params.id;
-			await feedbackService.deleteFeedback(id);
-			return res.status(204).send('Feedback Deleted');
-		} catch (err) {
-			console.error(err);
-		}
-	};
-	upvoteFeedback = async (req: any, res: any) => {
-		try {
-			const id: string = req.params.id;
-			await feedbackService.upvoteFeedback(id);
-		} catch (err) {
-			console.error(err);
-		}
-	};
+export class FeedbackController {
+  getFeedback = async (req: any, res: any) => {
+    try {
+      return res.status(200).json(await feedbackService.getFeedback());
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  getSingleFeedback = async (req: any, res: any) => {
+    try {
+      const id = req.params.id;
+      return res.status(200).json(await feedbackService.getSingleFeedback(id));
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  getSingleFeedbackComments = async (req: any, res: any) => {
+    try {
+      const id = req.params.id;
+      return res
+        .status(200)
+        .json(await feedbackService.getSingleFeedbackComments(id));
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  createFeedback = async (req: any, res: any) => {
+    try {
+      const postData = req.body;
+      await feedbackService.createFeedback(postData);
+      return res.status(201).json(feedbackService.getFeedback());
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  updateFeedback = async (req: any, res: any) => {
+    try {
+      const id: string = req.params.id;
+      const postData = req.body;
+      await feedbackService.updateFeedback(postData, id);
+      return res.status(200).json(await feedbackService.getFeedback());
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  deleteFeedback = async (req: any, res: any) => {
+    try {
+      const id: string = req.params.id;
+      await feedbackService.deleteFeedback(id);
+      return res.status(204).send("Feedback Deleted");
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  upvoteFeedback = async (req: any, res: any) => {
+    try {
+      const id: string = req.params.id;
+      await feedbackService.upvoteFeedback(id);
+    } catch (err) {
+      console.error(err);
+    }
+  };
 }
-
-module.exports = new FeedbackController();
