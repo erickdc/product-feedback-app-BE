49957825b3fa355535584b6aa5f3541fe46c5514
Erick <erickdcb10@gmail.com>
2021-10-13 20:31:31 -0600

    .

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,26 +5,32 @@ const FeedbackController = require("../controllers/feedbackController");
 const UserController = require("../controllers/UserController");
 const CommentController = require("../controllers/CommentController");
 
-const router: express.Router = express.Router();
+var router = express.Router();
 
 // FEEDBACK
 // Show all feedback
-router.get("/feedbackproduct", FeedbackController.getFeedback);
+router.get(["/", "/feedbackproduct"], FeedbackController.getFeedback);
 // Show one feedback
-router.get("/feedbackproduct/:id", FeedbackController.getSingleFeedback);
+router.get(
+  ["/:id", "/feedbackproduct/:id"],
+  FeedbackController.getSingleFeedback
+);
 // Update a feedback
-router.put("/feedbackproduct/:id", FeedbackController.updateFeedback);
+router.put(["/:id", "/feedbackproduct/:id"], FeedbackController.updateFeedback);
 // Show comments within feedback
 router.get(
-  "/feedbackproduct/:id/comments",
+  ["/:id/comments", "/feedbackproduct/:id/comments"],
   FeedbackController.getSingleFeedbackComments
 );
 // Add new feedback to database
-router.post("/feedbackproduct", FeedbackController.createFeedback);
+router.post(["/", "/feedbackproduct"], FeedbackController.createFeedback);
 // Delete feedback from database
-router.delete("/feedbackproduct/:id", FeedbackController.deleteFeedback);
+router.delete(
+  ["/:id", "/feedbackproduct/:id"],
+  FeedbackController.deleteFeedback
+);
 // Upvote Feedback
-router.put("/feedbackproduct/:id", FeedbackController.upvoteFeedback);
+router.put([":/id", "/feedbackproduct/:id"], FeedbackController.upvoteFeedback);
 // USER
 // Create User
 router.post("/users", UserController.createUser);
@@ -34,7 +40,7 @@ router.post("/users/login", UserController.loginUser);
 
 // COMMENTS
 router.post(
-  "/feedbackproduct/:id/comments",
+  ["/:id/comments", "/feedbackproduct/:id/comments"],
   Auth.verifyToken,
   CommentController.createComment
 );
